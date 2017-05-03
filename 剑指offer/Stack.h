035683c667801d591fa890863b6586e3a3abca3a
FuXin-1024 <1007160080@qq.com>
2017-05-03 10:24:12 +0800

    两个队列实现一个栈

--- 剑指offer/Stack.h
+++ 剑指offer/Stack.h
@@ -39,6 +39,38 @@ public:
 			q1.pop();
 		}
 	}
+
+	T top()
+	{
+		T tmp;
+		assert(q1.size() || q2.size());
+		//队1为空时，把队2出最后一个元素倒入队1
+		if (q1.empty())
+		{
+			while (q2.size() > 1)
+			{
+				q1.push(q2.front());
+				q2.pop();
+			}
+			tmp = q2.front();
+			q1.push(tmp);
+			q2.pop();
+			return tmp;
+		}
+		else
+		{
+			while (q1.size() > 1)
+			{
+				q2.push(q1.front());
+				q1.pop();
+			}
+			tmp = q1.front();
+			q2.push(tmp);
+			q1.pop();
+			return tmp;
+		}
+		
+	}
 protected:
 	queue<T> q1;
 	queue<T> q2;
@@ -50,11 +82,13 @@ void StackTest()
 	s.Push(1);
 	s.Push(2);
 	s.Push(3);
+	cout << s.top() << endl;
 	s.Pop();
-
+	cout << s.top() << endl;
 	s.Push(4);
 	s.Push(5);
+	cout << s.top() << endl;
 	s.Pop();
 	s.Pop();
-
+	cout << s.top() << endl;
 }
